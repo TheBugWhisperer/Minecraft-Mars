e991864c958ca8eec7b8268cfd27c2dfa23641c1
TheBugWhisperer <aurora@elmakers.com>
2020-12-05 09:55:16 -0800

    first working code

---
+++ src/main/java/com/elmakers/mine/bukkit/plugins/MarsPlugin.java
@@ -0,0 +1,28 @@
+package com.elmakers.mine.bukkit.plugins;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MarsPlugin extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Put here anything you want to happen when the server starts
+    	
+    	// Register our join event
+    	getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Put here anything you want to happen when the server stops
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        // Put here anything you want to happen when a player joins
+    	event.setJoinMessage("Good Luck!");
+    }
+}
