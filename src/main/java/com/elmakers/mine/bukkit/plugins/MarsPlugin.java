885b8d350b56c7f49f130d59fea5d3d2dce60078
TheBugWhisperer <aurora@elmakers.com>
2021-02-10 16:12:32 -0800

    Remove join message

--- src/main/java/com/elmakers/mine/bukkit/plugins/MarsPlugin.java
+++ src/main/java/com/elmakers/mine/bukkit/plugins/MarsPlugin.java
@@ -1,8 +1,6 @@
 package com.elmakers.mine.bukkit.plugins;
 
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,20 +9,11 @@ public class MarsPlugin extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         // Put here anything you want to happen when the server starts
-    	
-    	// Register our join event
-    	getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
         // Put here anything you want to happen when the server stops
-    }
-
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        // Put here anything you want to happen when a player joins
-    	event.setJoinMessage("Good Luck!");
     }
 
     @Override
